7a0d1ef3afd04c472f49b761c707b2cfec8cc10c
Girinath Selvendran <89913730+girinathselvendran@users.noreply.github.com>
2024-07-02 10:37:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -32,6 +37,7 @@ function App() {
         <Router>
           <div className="App">
             <Routes>
+              <Route path="/" element={<Navigate to="/login" replace />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               {user == "user" ? (
